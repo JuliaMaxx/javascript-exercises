5acd3c470fc3ba3178a2f84fcafaf40305af686d
JuliaMaxy <jjuliamaxxx@gmail.com>
2023-08-16 20:53:23 +0300

    Exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    let sum = 0;
+    if (n1 < 0 || n2 < 0 || typeof n2!=='number' || typeof n1!=='number') return 'ERROR';
+    for(let i = (n1 < n2? n1: n2); i <= (n2 > n1? n2: n1); i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
